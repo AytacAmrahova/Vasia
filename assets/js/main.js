476a1a7931fece715639f68adecb7d412f121cd2
AytacAmrahova <aytajamrahova@gmail.com>
2023-08-14 22:02:20 +0400

    Initial

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+$('.our-designers').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:7,
+            nav:true,
+            loop:false
+        }
+    }
+})
+$('.featured-products-items').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+$('.featured-products-items-2').owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:2,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
